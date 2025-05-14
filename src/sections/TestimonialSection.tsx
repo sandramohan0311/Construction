4149ea0c8e5872596e9a878738f0568a61a80508
sandramohan0311 <sandramohan0311@gmail.com>
2025-05-14 20:15:27 +0530

    feat(home page): added images and  arabic translate

--- src/sections/TestimonialSection.tsx
+++ src/sections/TestimonialSection.tsx
@@ -1,14 +1,18 @@
 import TestimonialComponent from "@/components/TestimonialComponent"
+import { useLanguage } from "@/contexts/LanguageContext";
 
 const TestimonialSection = () => {
+
+  const { t } = useLanguage();
+
   return (
     <div className='h-auto w-full flex flex-col gap-5 pb-10'>
-        <p className='lg:text-4xl text-2xl text-black font-medium w-full flex justify-center items-center'>Testimonials</p>
-        <p className='lg:text-xl text-md text-black font-medium w-full flex justify-center items-center'>Real stories from clients who’ve built success with us.</p>
+        <p className='lg:text-4xl text-2xl text-black font-medium w-full flex justify-center items-center'>{t("THdg")}</p>
+        <p className='lg:text-xl text-md text-black font-medium w-full flex justify-center items-center'>{t("TSubHdg")}</p>
         <div className="h-auto w-full flex lg:flex-row lg:justify-center flex-col items-center gap-5">
-            <TestimonialComponent TstlName='John Mathews' TstlPlace='Project Director, UrbanRise Developers' TstlDescription='"Working with [Company Name] was a seamless experience. Their attention to detail, timely delivery, and commitment to safety ensured our project exceeded expectations. They’re not just contractors — they’re trusted partners in growth."' />
-            <TestimonialComponent TstlName='John Mathews' TstlPlace='Project Director, UrbanRise Developers' TstlDescription='"Working with [Company Name] was a seamless experience. Their attention to detail, timely delivery, and commitment to safety ensured our project exceeded expectations. They’re not just contractors — they’re trusted partners in growth."' />
-            <TestimonialComponent TstlName='John Mathews' TstlPlace='Project Director, UrbanRise Developers' TstlDescription='"Working with [Company Name] was a seamless experience. Their attention to detail, timely delivery, and commitment to safety ensured our project exceeded expectations. They’re not just contractors — they’re trusted partners in growth."' />
+            <TestimonialComponent TstlName={t("TName")} TstlPlace={t("TPosition")} TstlDescription={t("Testimonial")} />
+            <TestimonialComponent TstlName={t("TName")} TstlPlace={t("TPosition")} TstlDescription={t("Testimonial")} />
+            <TestimonialComponent TstlName={t("TName")} TstlPlace={t("TPosition")} TstlDescription={t("Testimonial")} />    
         </div>
         
     </div>
--- src/sections/WelcomeSection.tsx
+++ src/sections/WelcomeSection.tsx
@@ -1,18 +1,22 @@
 import Com_image1 from '@/assets/image/hero1.png';
 import { Button } from "@/components/ui/button"
+import { useLanguage } from '@/contexts/LanguageContext';
 
 
 const WelcomeSection = () => {
+
+   const { t } = useLanguage();
+
   return (
     <div className='lg:h-[400px] h-auto w-full flex lg:flex-row lg:justify-center flex-col  '>
         <div className="h-full lg:w-[500px] w-full px-5 lg:px-0 bg-[#E0DFE4] flex items-end">
             <img className='h-full w-full object-cover' src={Com_image1} alt="" />
         </div>
         <div className="h-full lg:w-[500px] w-full px-3 py-10 flex flex-col gap-3">
-            <p className='text-32 font-medium'>Welcome to Saeed seddiqi construction </p>
-            <p className='lg:text-3xl text-xl font-bold'>Saeed seddiqi construction Co. LLC| Low Cost | Stunning Design | Enduring Trust</p>
-            <p className='text-24 font-normal text-[#626262]'>Established in the year 2018, Era Creatio Developers in Kuttikattor, Is A One Of The Top Player In The Category Builders & Developers In Kerala.</p>
-            <Button variant="outline" className='bg-black text-white w-44'>Learn More</Button>
+            <p className='text-32 font-medium'>{t("WelS1")} </p>
+            <p className='lg:text-3xl text-xl font-bold'>{t("WelS2")}</p>
+            <p className='text-24 font-normal text-[#626262]'>{t("WelS3")}</p>
+            <Button variant="outline" className='bg-black text-white w-44'>{t("WelS4")}</Button>
         </div>
       
     </div>
