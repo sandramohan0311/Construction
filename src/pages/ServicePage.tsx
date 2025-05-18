051a6bd9f5affe5399f4a5f29e584031c40f2bc2
sandramohan0311 <sandramohan0311@gmail.com>
2025-05-18 20:59:25 +0530

    feat(home page): linked to hosted backend

--- src/pages/ServicePage.tsx
+++ src/pages/ServicePage.tsx
@@ -3,8 +3,12 @@ import ServicePageSection from '@/sections/ServisePageSection';
 import { servicesData } from '@/constant/Service'; // constants file with 10 services
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { useLanguage } from '@/contexts/LanguageContext';
 
 const ServicePage = () => {
+
+  const { language } = useLanguage();
+
   const { type } = useParams<{ type: string }>();
 
   const service = servicesData.find(s => s.galleryType === type);
@@ -12,7 +16,7 @@ const ServicePage = () => {
   if (!service) return <p>Service not found</p>;
 
   return (
-    <div className="h-full w-full">
+    <div dir={language === "ar" ? "rtl" : "ltr"} className="h-full w-full">
       <Header/>
       <ServicePageSection
         titleKey={service.titleKey}
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,5 @@
 // URLS
-const BASE_URL: string = "http://localhost:5000";
+const BASE_URL: string = "https://saeedconstructionbackend.eracreatiodevelopers.com";
 
 export const API_URL: string = `${BASE_URL}/api/v1`;
 export const BASE_FILE_URL: string = `${BASE_URL}/uploads/`;
